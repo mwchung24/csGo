6e0097d0ec61dbd0330331160e6b50ccee2f1e8e
Martin Chung <mwchung24@gmail.com>
2017-09-06 16:37:03 -0400

    rendering item in modal

--- src/modal.jsx
+++ src/modal.jsx
@@ -3,16 +3,17 @@ import React, { Component } from 'react';
 class Modal extends Component {
   constructor(props) {
     super(props);
-
   }
 
   render () {
     if(this.props.open) {
-
       return (
         <div className="fullModal" onClick={ () => this.props.closeModal()}>
-          <div className="modalContent">
-            hi
+          <div className="modalContent" onClick = {(e) => e.stopPropagation()}>
+            <div className='item-name-modal'>
+              {this.props.children.market_hash_name}
+            </div>
+            <img className='skin-picture-modal' src={this.props.children.icon_url}></img>
           </div>
         </div>
       );
